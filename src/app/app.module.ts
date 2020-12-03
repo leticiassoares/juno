8ca2cb3a28b74ebc3f4e5f93506c1f073d208dec
Leticia Soares <69875352+leticiassoares@users.noreply.github.com>
2020-12-03 13:10:52 -0300

    prepara o ambiente, implementa a lib modelo, cria componentes header, nav e home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  ButtonModule,
+  CheckboxModule,
+  FormModule,
+  IconModule,
+  InputModule,
+  LabelModule,
+  RadioModule,
+} from '@cca-fab/cca-fab-components-common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './template/nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    IconModule,
+    FormModule,
+    InputModule,
+    CheckboxModule,
+    LabelModule,
+    RadioModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
